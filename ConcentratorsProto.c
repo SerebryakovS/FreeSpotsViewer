de4b088ab68c225b665fbe93ff7a24a62661065c
Stepan Serebryakov <serebryakov0906@gmail.com>
2024-08-06 06:55:41 +0000

    test modbus 1

--- ConcentratorsProto.c
+++ ConcentratorsProto.c
@@ -37,40 +37,59 @@ void RunModbusSlave(int32_t SlaveId, UartModule *_UartModule) {
     if (SlaveId < 0) {
         fprintf(stderr, "Failed to get correct device Id\n");
         return;
-    };
+    }
+
     modbus_t *ModbusContext;
     modbus_mapping_t *MbMapping;
     uint8_t Query[MODBUS_RTU_MAX_ADU_LENGTH];
+
     ModbusContext = modbus_new_rtu(RS485_UART_PORT_B, 9600, 'N', 8, 1);
-    modbus_set_slave(ModbusContext, SlaveId);
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 500000;  // 500 ms
-    modbus_set_response_timeout(ModbusContext, &timeout);
-    modbus_set_byte_timeout(ModbusContext, &timeout);
+    if (ModbusContext == NULL) {
+        fprintf(stderr, "Unable to create the libmodbus context\n");
+        return;
+    }
+
+    if (modbus_set_slave(ModbusContext, SlaveId) == -1) {
+        fprintf(stderr, "Failed to set slave ID: %s\n", modbus_strerror(errno));
+        modbus_free(ModbusContext);
+        return;
+    }
+
+    struct timeval Timeout;
+    Timeout.tv_sec = 0;
+    Timeout.tv_usec = 500000;  // 500 ms
+    modbus_set_response_timeout(ModbusContext, &Timeout);
+    modbus_set_byte_timeout(ModbusContext, &Timeout);
+
     MbMapping = modbus_mapping_new(0, 0, SENSORS_COUNT, 0);
     if (MbMapping == NULL) {
         fprintf(stderr, "Failed to allocate the mapping: %s\n", modbus_strerror(errno));
         modbus_free(ModbusContext);
         return;
-    };
+    }
+
     if (modbus_connect(ModbusContext) == -1) {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
         modbus_mapping_free(MbMapping);
         modbus_free(ModbusContext);
         return;
-    };
+    }
+
     printf("Running as Modbus Slave with ID %d...\n", SlaveId);
+
+    // Ensure initially in receive mode
+    digitalWrite(_UartModule->EnablePin, LOW);
+
     while (!IsMaster()) {
         fd_set ReadFds;
         FD_ZERO(&ReadFds);
         int SocketFd = modbus_get_socket(ModbusContext);
         FD_SET(SocketFd, &ReadFds);
-        int ret = select(SocketFd + 1, &ReadFds, NULL, NULL, &timeout);
-        if (ret == -1) {
+        int16_t SelectResult = select(SocketFd + 1, &ReadFds, NULL, NULL, &Timeout);
+        if (SelectResult == -1) {
+            perror("Select error");
             break;
-        } else if (ret > 0 && FD_ISSET(SocketFd, &ReadFds)) {
-            digitalWrite(_UartModule->EnablePin, LOW);
+        } else if (SelectResult > 0 && FD_ISSET(SocketFd, &ReadFds)) {
             int RequestLength = modbus_receive(ModbusContext, Query);
             if (RequestLength > 0) {
                 SensorData *CurrSensor = SensorsHead;
@@ -79,42 +98,58 @@ void RunModbusSlave(int32_t SlaveId, UartModule *_UartModule) {
                     MbMapping->tab_registers[Idx] = CurrSensor->Data;
                     CurrSensor = CurrSensor->NextSensor;
                     Idx++;
-                };
+                }
+
+                // Switch to transmit mode
                 digitalWrite(_UartModule->EnablePin, HIGH);
                 modbus_reply(ModbusContext, Query, RequestLength, MbMapping);
+                // Switch back to receive mode
                 digitalWrite(_UartModule->EnablePin, LOW);
             } else if (RequestLength == -1 && errno != ETIMEDOUT) {
                 fprintf(stderr, "Modbus receive error: %s\n", modbus_strerror(errno));
                 break;
-            };
-        };
+            }
+        }
         usleep(100000);
-    };
+    }
+
     modbus_mapping_free(MbMapping);
     modbus_close(ModbusContext);
     modbus_free(ModbusContext);
-};
+}
 
 void RunModbusMaster(UartModule *_UartModule) {
     modbus_t *ModbusContext;
     ConcentratorData Slaves[BSLAVES_COUNT] = {0};
     uint16_t TabReg[SENSORS_COUNT];
+
     ModbusContext = modbus_new_rtu(RS485_UART_PORT_B, 9600, 'N', 8, 1);
+    if (ModbusContext == NULL) {
+        fprintf(stderr, "Unable to create the libmodbus context\n");
+        return;
+    }
+
     if (modbus_connect(ModbusContext) == -1) {
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
         modbus_free(ModbusContext);
         return;
-    };
-    printf("Master run\n");
+    }
+
+    printf("Master running...\n");
+
     while (IsMaster()) {
         for (uint8_t SlaveId = 1; SlaveId <= BSLAVES_COUNT; ++SlaveId) {
             if (modbus_set_slave(ModbusContext, SlaveId) == -1) {
                 fprintf(stderr, "Failed to set slave ID: %s\n", modbus_strerror(errno));
                 continue;
-            };
+            }
+
+            // Switch to transmit mode
             digitalWrite(_UartModule->EnablePin, HIGH);
             int RequestLength = modbus_read_registers(ModbusContext, SENSOR_ZERO_ADDR, SENSORS_COUNT, TabReg);
+            // Switch back to receive mode
             digitalWrite(_UartModule->EnablePin, LOW);
+
             if (RequestLength == -1) {
                 fprintf(stderr, "Read failed for slave %d: %s\n", SlaveId, modbus_strerror(errno));
             } else {
@@ -122,14 +157,16 @@ void RunModbusMaster(UartModule *_UartModule) {
                 for (uint8_t Idx = 0; Idx < RequestLength; ++Idx) {
                     Slaves[SlaveId - 1].Sensors[Idx].Address = SENSOR_ZERO_ADDR + Idx;
                     Slaves[SlaveId - 1].Sensors[Idx].Data = TabReg[Idx];
-                };
-            };
-        };
+                }
+            }
+        }
         usleep(100000);
-    };
+    }
+
     modbus_close(ModbusContext);
     modbus_free(ModbusContext);
-};
+}
+
 
 void *SyncConcentratorsHandler(void *Arguments) {
     UartModule *_UartModule = (UartModule *)Arguments;
